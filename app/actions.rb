ea9f6b80b3e6c6ca2b924a1ac70a5c3871fb7ab3
Thuy-Linh Nguyen <thuylool@hotmail.com>
2023-05-27 22:36:43 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -50,6 +50,29 @@ post '/login' do
   end
 end
 
+post '/comments' do
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  comment.save
+
+  redirect(back)
+
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+
+  like.save
+
+  redirect(back)
+
+end
+
 
 get '/logout' do
   session[:user_id] = nil
@@ -79,4 +102,10 @@ end
 get '/finstagram_posts/:id' do
   @finstagram_post = FinstagramPost.find(params[:id])
   erb(:"finstagram_posts/show")
+end
+
+delete '/likes/:id' do
+  like =  Like.find(params[:id])
+  like.destroy
+  redirect(back)
 end
